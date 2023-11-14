ab0182b9250df89bdee2c0a30bc35de6d27d8a7e
metmoiquatroi <vinhlqpc05646@fpt.edu.vn>
2023-11-14 08:08:57 +0700

    giaodiendangnhap

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,13 +29,135 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btdangnhap = new System.Windows.Forms.Button();
+            this.bthuy = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cbhienpass = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(114, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG NHẬP HỆ THỐNG";
+            // 
+            // btdangnhap
+            // 
+            this.btdangnhap.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btdangnhap.Location = new System.Drawing.Point(216, 430);
+            this.btdangnhap.Name = "btdangnhap";
+            this.btdangnhap.Size = new System.Drawing.Size(108, 34);
+            this.btdangnhap.TabIndex = 1;
+            this.btdangnhap.Text = "Đăng nhập";
+            this.btdangnhap.UseVisualStyleBackColor = true;
+            // 
+            // bthuy
+            // 
+            this.bthuy.Location = new System.Drawing.Point(369, 430);
+            this.bthuy.Name = "bthuy";
+            this.bthuy.Size = new System.Drawing.Size(108, 34);
+            this.bthuy.TabIndex = 2;
+            this.bthuy.Text = "Thoát";
+            this.bthuy.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(247, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(247, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Pass:";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(251, 214);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(226, 22);
+            this.txtuser.TabIndex = 5;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(251, 311);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(226, 22);
+            this.txtpass.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.pngtree_flat_restaurant_illustration_png_image_1568130;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 131);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(184, 252);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cbhienpass
+            // 
+            this.cbhienpass.AutoSize = true;
+            this.cbhienpass.Location = new System.Drawing.Point(251, 350);
+            this.cbhienpass.Name = "cbhienpass";
+            this.cbhienpass.Size = new System.Drawing.Size(121, 21);
+            this.cbhienpass.TabIndex = 8;
+            this.cbhienpass.Text = "Hiện mật khẩu";
+            this.cbhienpass.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(526, 526);
+            this.Controls.Add(this.cbhienpass);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bthuy);
+            this.Controls.Add(this.btdangnhap);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btdangnhap;
+        private System.Windows.Forms.Button bthuy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox cbhienpass;
     }
 }
 
